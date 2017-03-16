0842f7c24bf58f3a27acc004ba75960abbcb66b3
mnemosdev <lorenzopieri.lavoro@gmail.com>
2017-03-16 22:56:21 +0100

    added request handlers

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require("express");
 var moment = require("moment");
 
 var app = express();
-
+app.get("/", function(req, res){
+	res.send("/timestamp/{date in unix or string}");
+});
+app.get("/timestamp", function(req, res){
+	res.send("/timestamp/{date in unix or string}");
+});
 app.get("/timestamp/:date", function(req, res){
 		var date = req.params.date;
 
@@ -20,7 +25,10 @@ app.get("/timestamp/:date", function(req, res){
 				});
 		}
 	});
-
+app.get("*", function(req, res){
+	res.status = 404;
+	res.send("page not found");
+});
 app.listen(process.argv[2], function(){
 	console.log("waiting for requests");
 });
